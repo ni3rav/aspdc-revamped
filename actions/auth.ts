871d43b5c10693dc1912a5d03143e44fd07b326b
ni3rav <mahetan35@gmail.com>
2025-08-11 10:02:41 +0530

    basic login done

--- actions/auth.ts
+++ actions/auth.ts
@@ -4,40 +4,29 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { createClient } from "@/supabase/server";
+import { authSchema } from "@/validations/auth";
 
 export async function login(formData: FormData) {
   const supabase = await createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
+  let data = {
     email: formData.get("email") as string,
     password: formData.get("password") as string,
   };
 
+  console.log(data);
+  const parsedData = authSchema.safeParse(data);
+
+  if (!parsedData.success) {
+    console.log("error in zod validation");
+    redirect("/");
+  }
+
+  data = parsedData.data;
   const { error } = await supabase.auth.signInWithPassword(data);
 
   if (error) {
-    redirect("/error");
-  }
-
-  revalidatePath("/", "layout");
-  redirect("/admin");
-}
-
-export async function signup(formData: FormData) {
-  const supabase = await createClient();
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
-  };
-
-  const { error } = await supabase.auth.signUp(data);
-
-  if (error) {
+    console.log(error);
     redirect("/error");
   }
 
---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "@/components/login-form";
+import React from "react";
+
+function page() {
+  return (
+    <div className="h-screen grid place-items-center">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default page;
