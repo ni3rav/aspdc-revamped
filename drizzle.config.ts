84d251a2da4c76c545eba7bdeece04f7713049cb
ni3rav <mahetan35@gmail.com>
2025-08-19 23:10:14 +0530

    fix: add setup drizzle on server side and add better auth

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,11 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { defineConfig } from 'drizzle-kit'
 
+config({ path: '.env' })
+
 export default defineConfig({
-    out: './drizzle',
     schema: './db/schema.ts',
+    out: './migrations',
     dialect: 'postgresql',
     dbCredentials: {
         url: process.env.DATABASE_URL!,
