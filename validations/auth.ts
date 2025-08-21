97716d48a050671a98df653d956b1079d230e19a
ni3rav <mahetan35@gmail.com>
2025-08-21 21:30:40 +0530

    wip: cleanup shit

--- validations/auth.ts
+++
@@ -1,6 +0,0 @@
-import * as z from 'zod'
-
-export const authSchema = z.object({
-    email: z.email('enter a valid email'),
-    password: z.string().min(8, 'password must be 8 characters long'),
-})
