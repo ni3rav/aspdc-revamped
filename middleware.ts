b80336a531b361139eca9081e84092f4f1eb107d
ni3rav <mahetan35@gmail.com>
2025-08-21 23:14:05 +0530

    fix: pnpm lock file and middleware should  now redirect to login route than the hosted signin page

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,23 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
 const isProtectedRoute = createRouteMatcher(['/admin(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
-    if (isProtectedRoute(req)) await auth.protect()
+    if (isProtectedRoute(req)) {
+        try {
+            await auth.protect()
+        } catch (err) {
+            console.log(err)
+            const loginUrl = new URL('/login', req.url)
+            return NextResponse.redirect(loginUrl)
+        }
+    }
 })
+
 export const config = {
     matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
         '/(api|trpc)(.*)',
     ],
 }
