5a5229d062ed953679e7b97fe7068a604372ec89
ni3rav <mahetan35@gmail.com>
2025-09-02 00:12:11 +0530

    fix: have to keep this clerk shit in root :cry-emoji

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,5 @@
 import type React from 'react'
 import { FloatingNavbar } from '@/components/admin/floating-navbar'
-import { ClerkProvider } from '@clerk/nextjs'
 
 export default function AdminLayout({
     children,
@@ -8,19 +7,11 @@ export default function AdminLayout({
     children: React.ReactNode
 }) {
     return (
-        <ClerkProvider
-            appearance={{
-                elements: {
-                    footer: 'hidden',
-                },
-            }}
-        >
-            <div className="bg-background flex min-h-screen min-w-screen items-center justify-center">
-                <FloatingNavbar basePath="/admin" />
-                <main className="max-h-10/12 w-3/4 overflow-x-hidden overflow-y-scroll px-4 pt-20 pb-8">
-                    {children}
-                </main>
-            </div>
-        </ClerkProvider>
+        <div className="bg-background flex min-h-screen min-w-screen items-center justify-center">
+            <FloatingNavbar basePath="/admin" />
+            <main className="max-h-10/12 w-3/4 overflow-x-hidden overflow-y-scroll px-4 pt-20 pb-8">
+                {children}
+            </main>
+        </div>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import QueryProvider from '@/lib/query-provider'
 import Navbar from '@/components/Navbar'
 import DotGrid from '@/components/bits/DotGrid/DotGrid'
 import Footer from '@/components/Footer'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const spaceGrotesk = Space_Grotesk({
     variable: '--font-space-grotesk',
@@ -23,27 +24,35 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <html lang="en">
-            <body
-                className={`${spaceGrotesk.className} secnone overflow-x-hidden antialiased`}
-            >
-                <Toaster richColors position="top-right" />
-                <QueryProvider>
-                    <div className="relative w-screen">
-                        <div className="fixed inset-0 -z-10">
-                            <DotGrid
-                                dotSize={10}
-                                baseColor="#111111"
-                                activeColor="#23C55E"
-                                className="h-full w-full"
-                            />
+        <ClerkProvider
+            appearance={{
+                elements: {
+                    footer: 'hidden',
+                },
+            }}
+        >
+            <html lang="en">
+                <body
+                    className={`${spaceGrotesk.className} secnone overflow-x-hidden antialiased`}
+                >
+                    <Toaster richColors position="top-right" />
+                    <QueryProvider>
+                        <div className="relative w-screen">
+                            <div className="fixed inset-0 -z-10">
+                                <DotGrid
+                                    dotSize={10}
+                                    baseColor="#111111"
+                                    activeColor="#23C55E"
+                                    className="h-full w-full"
+                                />
+                            </div>
+                            <Navbar />
                         </div>
-                        <Navbar />
-                    </div>
-                    <div className="mt-9 sm:mt-0">{children}</div>
-                    <Footer />
-                </QueryProvider>
-            </body>
-        </html>
+                        <div className="mt-9 sm:mt-0">{children}</div>
+                        <Footer />
+                    </QueryProvider>
+                </body>
+            </html>
+        </ClerkProvider>
     )
 }
