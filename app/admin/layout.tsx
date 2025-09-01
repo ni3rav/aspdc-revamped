82638e13d8e6ce62814974bd2a54d272445864a4
ni3rav <mahetan35@gmail.com>
2025-09-01 23:13:46 +0530

    edit: move clerk provider to admin's layour only edit: customize scrollbar, smooth scroll

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,6 @@
 import type React from 'react'
 import { FloatingNavbar } from '@/components/admin/floating-navbar'
+import { ClerkProvider } from '@clerk/nextjs'
 
 export default function AdminLayout({
     children,
@@ -7,11 +8,19 @@ export default function AdminLayout({
     children: React.ReactNode
 }) {
     return (
-        <div className="bg-background flex min-h-screen min-w-screen items-center justify-center">
-            <FloatingNavbar basePath="/admin" />
-            <main className="max-h-10/12 w-3/4 overflow-x-hidden overflow-y-scroll px-4 pt-20 pb-8">
-                {children}
-            </main>
-        </div>
+        <ClerkProvider
+            appearance={{
+                elements: {
+                    footer: 'hidden',
+                },
+            }}
+        >
+            <div className="bg-background flex min-h-screen min-w-screen items-center justify-center">
+                <FloatingNavbar basePath="/admin" />
+                <main className="max-h-10/12 w-3/4 overflow-x-hidden overflow-y-scroll px-4 pt-20 pb-8">
+                    {children}
+                </main>
+            </div>
+        </ClerkProvider>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Space_Grotesk } from 'next/font/google'
 import { Toaster } from '@/components/ui/sonner'
 import './globals.css'
 import QueryProvider from '@/lib/query-provider'
-import { ClerkProvider } from '@clerk/nextjs'
 import Navbar from '@/components/Navbar'
 import DotGrid from '@/components/bits/DotGrid/DotGrid'
 import Footer from '@/components/Footer'
@@ -24,35 +23,27 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <ClerkProvider
-            appearance={{
-                elements: {
-                    footer: 'hidden',
-                },
-            }}
-        >
-            <html lang="en">
-                <body
-                    className={`${spaceGrotesk.className} secnone overflow-x-hidden antialiased`}
-                >
-                    <Toaster richColors position="top-right" />
-                    <QueryProvider>
-                        <div className="relative w-screen">
-                            <div className="fixed inset-0 -z-10">
-                                <DotGrid
-                                    dotSize={10}
-                                    baseColor="#111111"
-                                    activeColor="#23C55E"
-                                    className="h-full w-full"
-                                />
-                            </div>
-                            <Navbar />
+        <html lang="en">
+            <body
+                className={`${spaceGrotesk.className} secnone overflow-x-hidden antialiased`}
+            >
+                <Toaster richColors position="top-right" />
+                <QueryProvider>
+                    <div className="relative w-screen">
+                        <div className="fixed inset-0 -z-10">
+                            <DotGrid
+                                dotSize={10}
+                                baseColor="#111111"
+                                activeColor="#23C55E"
+                                className="h-full w-full"
+                            />
                         </div>
-                        <div className="mt-9 sm:mt-0">{children}</div>
-                        <Footer />
-                    </QueryProvider>
-                </body>
-            </html>
-        </ClerkProvider>
+                        <Navbar />
+                    </div>
+                    <div className="mt-9 sm:mt-0">{children}</div>
+                    <Footer />
+                </QueryProvider>
+            </body>
+        </html>
     )
 }
