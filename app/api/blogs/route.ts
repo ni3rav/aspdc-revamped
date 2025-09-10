613ccfbe3b44edf7ac74a96b58da9aa982ba498d
RudraPatel5435 <rudranpatel0980@gmail.com>
2025-09-10 18:57:18 +0530

    add: applied rate limit to all pages

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -1,8 +1,19 @@
 import { db } from '@/db/drizzle'
 import { blogs } from '@/db/schema'
 import { desc } from 'drizzle-orm'
+import { rateLimit } from '@/lib/rate-limit'
+import { NextRequest } from 'next/server'
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+    const ip = req.headers.get('x-forwarded-for') ?? 'anonymous'
+    const { success, reset, remaining } = await rateLimit.limit(ip)
+
+    if (!success) {
+        return new Response('rate limit exceeded, try again later', {
+            status: 429,
+        })
+    }
+
     try {
         const data = await db
             .select()
--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,8 +1,19 @@
 import { db } from '@/db/drizzle'
 import { events } from '@/db/schema'
 import { desc } from 'drizzle-orm'
+import { rateLimit } from '@/lib/rate-limit'
+import { NextRequest } from 'next/server'
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+    const ip = req.headers.get('x-forwarded-for') ?? 'anonymous'
+    const { success, reset, remaining } = await rateLimit.limit(ip)
+
+    if (!success) {
+        return new Response('rate limit exceeded, try again later', {
+            status: 429,
+        })
+    }
+
     try {
         const rows = await db.select().from(events).orderBy(desc(events.date))
         const data = rows.map((row) => ({
